ad7cc186d33b855b6b7893ecf4b40a3977336f1b
Jherimum <341254+jherimum@users.noreply.github.com>
2022-12-07 13:45:06 -0300

    rename

--- tests/models/mod.rs
+++ tests/models/mod.rs
@@ -1,7 +1,7 @@
 use crate::common::TestContext;
 
-mod password_test;
-mod workspace_test;
+mod password_it;
+mod workspace_it;
 
 pub fn build_context(db_name: &str) -> TestContext {
     TestContext::new("postgresql://wmessage:wmessage@localhost:6543", db_name)
