4869ff56969a8f32ce919621226d94256aaede67
Jherimum <341254+jherimum@users.noreply.github.com>
2022-12-15 09:19:49 -0300

    worspace routes

--- src/app/routes/workspace_routes.rs
+++ src/app/routes/workspace_routes.rs
@@ -1,17 +1,5 @@
-use actix_web::{web::get, Scope};
+use actix_web::Scope;
 
 pub fn routes() -> Scope {
-    let workspaces = web::resource("")
-        .route(post().to(create))
-        .route(get().to(all))
-        .name("channel");
-    let workspace = web::resource("/{workspace_id}")
-        .route(get().to(find))
-        .route(patch().to(update));
-
     Scope::new("/workspaces")
-        .service(workspace)
-        .service(workspaces)
 }
-
-fn find() {}
